f65ff2abbff3ff831a0af43f39ed789282934129
IliaYusov <ouster2k@gmail.com>
2020-09-30 01:05:05 +0300

    fixed bug

--- cook_book.py
+++ cook_book.py
@@ -1,19 +1,23 @@
 def read_recipes(file_name='recipes.txt'):
     book = {}
-    with open(file_name) as f:
+    with open(file_name, 'r', encoding='utf-8') as f:
         end_of_file = True
         while end_of_file:
             recipe_name = f.readline().strip()
             ingredient_count = int(f.readline())
-            recipe = {}
             recipe_list = []
             for _ in range(ingredient_count):
-                print(recipe_list)
-                recipe['ingredient_name'], recipe['quantity'], recipe['measure'] = f.readline().strip().split('|')
+                recipe_line = f.readline().split('|')
+                recipe = {
+                    'ingredient_name': recipe_line[0].strip(),
+                    'quantity': int(recipe_line[1]),
+                    'measure': recipe_line[2].strip()
+                }
                 recipe_list.append(recipe)
-            book[recipe_name] = recipe_list[:]
+            book[recipe_name] = recipe_list
             end_of_file = f.readline()
     return book
+
 
 def get_shop_list_by_dishes(dishes, person_count):
     if person_count > 0:
@@ -23,14 +27,14 @@ def get_shop_list_by_dishes(dishes, person_count):
             if recipe in dishes:
                 for ingredient in cook_book[recipe]:
                     if ingredient['ingredient_name'] in shop_list:
-                        shop_list[ingredient['ingredient_name']]['quantity'] += ingredient['measure'] * person_count
+                        shop_list[ingredient['ingredient_name']]['quantity'] += ingredient['quantity'] * person_count
                     else:
-                        shop_list[ingredient['ingredient_name']] = {'meausure': ingredient['measure'], 'quantity': ingredient['measure'] * person_count}       
+                        shop_list[ingredient['ingredient_name']] = {'measure': ingredient['measure'],
+                                                                    'quantity': ingredient['quantity'] * person_count}
+        return shop_list
     else:
         return 'Wrong person number'
-        
 
-cook_book = read_recipes()
-##shop_list = get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 2)
-##print(cook_book.items())
-##print(shop_list)
+
+shopping_list = get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 2)
+print(shop_list)
